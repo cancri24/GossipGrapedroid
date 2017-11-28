b7107ba3575914232a95c3feb4f52e27e431b2ee
cancri24 <cirwindiehl@gmail.com>
2017-11-28 10:26:19 -0500

    things come on in a time thing

--- app/src/main/java/mobile/cse/gossipgrapedroid/MainActivity.java
+++ app/src/main/java/mobile/cse/gossipgrapedroid/MainActivity.java
@@ -1,21 +1,23 @@
 package mobile.cse.gossipgrapedroid;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
-
 import java.util.Random;
-
-//TODO: add at one more control and text input
 
 public class MainActivity extends AppCompatActivity {
     public static Random rand = new Random();
     private String gossipType = "both";
     private boolean swarmMode = false;
+    int counter = 0;
+    String theNews = "I have so many juicy secrets. What would you like to know?";
     TextView txView;
+    ConstraintLayout screen;
     EditText customName;
 
     @Override
@@ -24,6 +26,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         txView = (TextView) findViewById(R.id.gossipBox);
         customName = (EditText) findViewById(R.id.customName);
+        screen = (ConstraintLayout) findViewById(R.id.thescreen);
+
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(25);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(!txView.getText().equals(theNews)) {
+                                    txView.setText(theNews.substring(0, counter));
+                                    counter++;
+                                }
+                                else counter = 0;
+                            }
+                        });
+                    }
+                }
+                catch (InterruptedException e) {}
+            }
+        };
+        t.start();
     }
 
     public void onRadioButtonClicked(View view) {
@@ -52,10 +79,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void generateGossip(View view) {
         String name = String.valueOf(customName.getText());
-        txView.setText(Gossip.getGossip(gossipType, swarmMode, name));
+        theNews = Gossip.getGossip(gossipType, swarmMode, name);
     }
 
     public void toggleSwarmMode(View view) {
         swarmMode = !swarmMode;
+        if(swarmMode) screen.setBackgroundColor(Color.YELLOW);
+        else screen.setBackgroundColor(Color.WHITE);
+
     }
 }
